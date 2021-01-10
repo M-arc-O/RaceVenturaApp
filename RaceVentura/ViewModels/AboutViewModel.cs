e9183aae0a1381fc51bcbefed1e45b26ce8b2474
Marco Trapman <m.trapman@gmail.com>
2021-01-10 11:09:10 +0100

    Updated about view. Disabled result button while race is active.

--- RaceVentura/ViewModels/AboutViewModel.cs
+++ RaceVentura/ViewModels/AboutViewModel.cs
@@ -9,7 +9,7 @@ namespace RaceVentura.ViewModels
         public AboutViewModel()
         {
             Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://raceventura.nl"));
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://raceventura.nl/#/appsupport"));
         }
 
         public ICommand OpenWebCommand { get; }
